6826eae66e40533f9e9f2cfb9157c4efa1381d08
ShamsTanweer <shamstanweer27@gmail.com>
2021-10-17 01:09:03 +0530

    Modified with the help of comment video

--- app.js
+++ app.js
@@ -151,6 +151,7 @@ app.get("/about", function(req, res){
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
-} app.listen(3000, function() {
+} 
+app.listen(3000, function() {
   console.log("Server has started Successfully.");
 });
